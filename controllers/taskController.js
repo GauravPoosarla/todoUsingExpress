919325c3191aa90de04d493f7b86dc745c791cdc
GauravPoosarla <poosarlagaurav@gmail.com>
2023-02-02 13:03:27 +0530

    fix: update controller of task

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -11,46 +11,20 @@ exports.getAlltasks = async (req, res) => {
 };
 
 exports.getTaskById = async (req, res) => {
-  const schema  = joi.object({
-    id: joi.number().required()
-  });
-
   const id = req.params.id;
-  const {error} = schema.validate({id});
-  if (error) {
-    res.status(400).json({message: error.details[0].message});
-    return;
-  }
   const result = await services.getTaskById(id);
   res.json({result});
 };
 
 exports.postTask = async (req, res) => { 
-  const schema  = joi.object({
-    taskName: joi.string().required()
-  });
   let task = req.body;
-  const {error} = schema.validate(task);
-  if (error) {
-    res.status(400).json({message: error.details[0].message});
-    return;
-  }
-  console.log(task);
   task['isCompleted'] = false;
   const result = await services.postTask(task);
   res.send(result);
 };
 
 exports.updateTask = async (req, res) => {
-  const schema = joi.object({
-    id: joi.number().required()
-  });
   const id = req.params.id;
-  const {error} = schema.validate(id);
-  if (error) {
-    res.status(400).json({message: error.details[0].message});
-    return;
-  }
   const result = await services.updateTask(id);
   res.json({result});
 };
--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const routes = require('./routes/taskRoutes');
 
 const app = express();
 
-
 app.use(express.json());
 app.use(routes);
-
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
